d33be8c01b34da36fb89989f2999d5f409c9d0bc
Daniel Cunha <danielbrandaodacunha@gmail.com>
2020-03-09 15:57:34 -0300

    Tutorial step 3

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def home(request):
-  return HttpResponse("Hello, world. You've reached the polls page")
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  output = ', '.join([q.question_text for q in latest_question_list])
+  return HttpResponse(output)
+
+  # return HttpResponse("Hello, world. You've reached the polls page")
 
 def detail(request, question_id):
   return HttpResponse("You're looking at question {}".format(question_id))
